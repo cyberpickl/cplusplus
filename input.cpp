20bd84b9aeb651f186071a8d261a9d70faaa58f5
shaina doraiswamy <shainadoraiswamy2002@outlook.com>
2026-03-24 15:14:14 +0530

    why should names have all the fun

--- input.cpp
+++ input.cpp
@@ -4,11 +4,16 @@
 int main(){
 
     std::string name;
+    int age;
 
     std:: cout << "What's your name: ";
     std::cin>> name;
 
-    std::cout<<"Hello "<<name;
+    std::cout<<"How old are you?: ";
+    std::cin>>age; 
+
+    std::cout<<"Hello "<<name<<'\n';
+    std::cout<<"You are "<<age<<" years old"<<'\n';
 
 
 
