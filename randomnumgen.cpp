18b216d87ab19435e25bac071f69628c5676b688
shaina doraiswamy <shainadoraiswamy19@gmail.com>
2026-03-25 12:29:15 +0530

    Random number generation using a single dice as an excercise

---
+++ randomnumgen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <ctime> //helps generate randoms i think
+
+int main()
+{
+    //pseudo - random = NOT truly random (but close)
+    srand(time(NULL)); // use the current time as the seed so the random numbers are different every run
+    
+
+    int num = (rand() % 6) + 1; // % 6 gives a remainder thats 0-6, but on a dice we dont have 6 so add 1
+    std::cout<<num;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
